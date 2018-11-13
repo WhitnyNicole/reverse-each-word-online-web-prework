cbf4fdb0775a7089ca7e78e365ad8364d0f74487
Whitny Edwards <wne4e2@gmail.com>
2018-11-13 02:04:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
-# reverse_each_word = "Hello there, and how are you?"
-
-
-
-# reverse_each_word.each do |word|
-#   puts word.reverse 
-# end 
 
 def reverse_each_word(array)
   array.split(" ").collect {|x| x.reverse!}.join(" ")
